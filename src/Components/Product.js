4ec4bacd7d05dd13725d6021fce83941b5963312
Lie Man <lieman@MacBook-Pro-de-Lie.local>
2018-08-09 14:52:22 +0200

    Product page

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import  { Carousel, CarouselInner, CarouselItem, View } from 'mdbreact';
 
 // Import Images 
 import slide from '../img/product/slide.png';
@@ -21,39 +20,36 @@ class Product extends Component {
           </div>
 
           <div className="container">
-            <div className="row">
+            <div className="row mt-5">
               {/* Carousel */}
-              <div className="slider col-lg-6 col-md-6 col-sm-12 col-12">
-                <Carousel
-                activeItem={1}
-                length={4}
-                showControls={true}
-                showIndicators={false}
-                className="z-depth-1">
-                <CarouselInner>
-                  <CarouselItem itemId="1">
-                    <View>
-                      <img className="d-block w-100" src={slide} alt="First slide" />
-                    </View>
-                  </CarouselItem>
-                  <CarouselItem itemId="2">
-                    <View>
-                      <img className="d-block w-100" src={slide1} alt="Second slide" />
-                    </View>
-                  </CarouselItem>
-                </CarouselInner>
-              </Carousel>
+              <div id="carouselExampleControls" className="carousel slide col-lg-6" data-ride="carousel">
+                <div className="carousel-inner">
+                  <div className="carousel-item active">
+                    <img className="d-block img-fluid" src={slide} alt="First slide" />
+                  </div>
+                  <div className="carousel-item">
+                    <img className="d-block img-fluid" src={slide1} alt="Second slide" />
+                  </div>
+                </div>
+                <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                  <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span className="sr-only">Previous</span>
+                </a>
+                <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                  <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span className="sr-only">Next</span>
+                </a>
               </div>
               {/* /Carousel */}
 
               <div className="crsl-details col-lg-6 col-md-6 col-sm-12 col-12">
                 <h3>Ave classic sweatshirt</h3>
                 <p>
-                  <span><i class="fas fa-star"></i></span>
-                  <span><i class="fas fa-star"></i></span>
-                  <span><i class="fas fa-star"></i></span>
-                  <span><i class="fas fa-star"></i></span>
-                  <span><i class="fas fa-star"></i></span> &nbsp;
+                  <span className="star-1 star"><i class="fas fa-star"></i></span>
+                  <span className="star-2 star"><i class="fas fa-star"></i></span>
+                  <span className="star-3 star"><i class="fas fa-star"></i></span>
+                  <span className="star-4 star"><i class="fas fa-star"></i></span>
+                  <span className="star-5 star"><i class="fas fa-star"></i></span> &nbsp;
                   <span>3 Review(s)</span>&nbsp; | &nbsp;
                   <span>Add a Review</span>&nbsp; | &nbsp;
                   <span>Share : &nbsp;
@@ -74,7 +70,7 @@ class Product extends Component {
                 <p className="mb-0"><span className="font-weight-bold">Product Code</span> : <span> #499577</span></p>
                 <p className="mb-0"><span className="font-weight-bold">Tags</span> : <span className="ave">Classic, Casual, V-neck, Loose</span></p>
 
-                <p>Donec sem lorem laoreet tempor un risus vitae, rutrum sodales nibh suspendisse est <br/> congue metus nunc, id viverra elit loreti rhoncus quis consecteur es. Donec pulvinar <br/> tempor lorem a pretium justo interdum.</p>
+                <p className="properties mt-2">Donec sem lorem laoreet tempor un risus vitae, rutrum sodales nibh suspendisse est <br/> congue metus nunc, id viverra elit loreti rhoncus quis consecteur es. Donec pulvinar <br/> tempor lorem a pretium justo interdum.</p>
                 <p className="mb-0">- Elasticated cuffs</p>
                 <p className="mb-0">- Casual fit</p>
                 <p className="mb-0">- 100% Cotton</p>
@@ -84,43 +80,100 @@ class Product extends Component {
                 <form action="">
                   <div className="selections d-flex flex-wrap">
                     <div>
-                        <label htmlFor="COLOUR">COLOUR</label> <br/>
-                        <select name="colour" id="colour" class="browser-default">
+                        <label htmlFor="COLOUR" className="font-weight-normal">COLOUR</label> <br/>
+                        <select name="colour" id="colour" dir="ltr">
                           <option value="" disabled selected>Select Colour</option>
                           <option value="1">Blue</option>
                           <option value="2">Black</option>
                           <option value="3">White</option>
                           <option value="4">Red</option>
                         </select>
-                      </div>
+                    </div>
                       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                      <div>
-                        <label htmlFor="SIZE">SIZE</label> <br/>
-                        <select name="size" id="size" class="browser-default">
-                          <option value="" disabled selected>Select Size</option>
-                          <option value="1">M</option>
-                          <option value="2">L</option>
-                          <option value="3">XL</option>
-                        </select>
-                      </div>
-                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                      <div>
-                        <label htmlFor="QTY">QTY</label> <br/>
-                        <input type="number" name="quantity" min="1" max="10" placeholder="1" />
-                      </div>
+                    <div>
+                      <label htmlFor="SIZE" className="font-weight-normal">SIZE</label> <br/>
+                      <select name="size" id="size">
+                        <option value="" disabled selected>Select Size</option>
+                        <option value="1">M</option>
+                        <option value="2">L</option>
+                        <option value="3">XL</option>
+                      </select>
+                    </div>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <div>
+                      <label htmlFor="QTY" className="font-weight-normal">QTY</label> <br/>
+                      <input type="number" name="quantity" min="1" max="10" placeholder="1" id="quantity" />
+                    </div>
                   </div>
                 </form>
 
-                <div className="mt-4">
-                  <button><i class="fas fa-shopping-cart"></i> &nbsp; ADD TO CART</button>
+                <div className="mt-5">
+                  <button className="btn-add"><i className="fas fa-shopping-cart"></i> &nbsp; ADD TO CART</button>
                   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                  <button><i class="far fa-heart"> &nbsp; ADD TO LOOKBOOK</i></button>
+                  <button className="btn-add"><i className="fas fa-heart"></i> &nbsp; ADD TO LOOKBOOK</button>
                 </div>
 
-                <p className="mt-5"><i class="fas fa-exchange-alt"></i> ADD TO COMPARE</p>
+                <p className="mt-5"><i className="fas fa-exchange-alt"></i> ADD TO COMPARE</p>
               </div>
 
-      
+              <div className="w-100 mt-4">
+                <ul className="nav nav-pills mb-3 bg-light" id="pills-tab" role="tablist">
+
+                  <li className="nav-item active-tab">
+                    <a className="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-description" role="tab" aria-controls="pills-description" aria-selected="true">DESCRIPTION</a>
+                  </li>
+
+                  <li className="nav-item">
+                    <a className="nav-link" id="pills-home-tab" data-toggle="pill" href="#pills-video" role="tab" aria-controls="pills-video" aria-selected="false">VIDEO</a>
+                  </li>
+
+                  <li className="nav-item">
+                    <a className="nav-link" id="pills-home-tab" data-toggle="pill" href="#pills-size" role="tab" aria-controls="pills-size" aria-selected="false">SIZE & PECS</a>
+                  </li>
+
+                  <li className="nav-item">
+                    <a className="nav-link" id="pills-home-tab" data-toggle="pill" href="#pills-delivery" role="tab" aria-controls="pills-delivery" aria-selected="false">DELIVERY & RETURNS</a>
+                  </li>
+
+                  <li className="nav-item">
+                    <a className="nav-link" id="pills-home-tab" data-toggle="pill" href="#pills-reviews" role="tab" aria-controls="pills-reviews" aria-selected="false">REVIEWS</a>
+                  </li>
+
+                </ul>
+
+                <div className="tab-content" id="pills-tabContent">
+                  <div className="tab-pane fade show active" id="pills-description" role="tabpanel" aria-labelledby="pills-description-tab">
+                    <h3>Description</h3>
+                    <p className="properties">Sed ut mi mollis, consequat nulla lacinia, hendrerit turpis. Nulla sapien magna, interdum quis pretium nec, pharetra at felis. Curabitur dictum sapien est, eget ultricies turpis <br/> porta vel. Nam suscipit nec lacus sed imperdiet. Vestibulum a purus risus. Nulla et dictum augue, nec efficitur mi. Nam sit amet pretium elit. Aliquam congue, ligula id <br/> vehicula vestibulum, orci ex vulputate lacus, ac malesuada elit dolor eget ex. Sed quis aliquet risus, ut cursus lectus. In eget lorem tellus.</p>
+                    <p className="properties">Quisque eleifend varius nisi nec sagittis. Nulla ullamcorper imperdiet justo, ut venenatis purus lobortis ut. Nunc sagittis urna et hendrerit sodales. Nunc molestie risus nec <br/> fringilla lacinia. Nulla facilisi. Etiam neque velit, tristique eget sollicitudin eget, placerat at metus. Proin dictum lobortis velit, id suscipit orci faucibus ut. Aliquam erat volut- <br/> pat. Vivamus feugiat justo in diam placerat, id dignissim elit auctor. Vestibulum scelerisque sem et lobortis ultricies. Morbi suscipit nulla urna. Suspendisse potenti. Nullam <br/> varius quam sed nisl dignissim, vel faucibus ipsum blandit. Vivamus at suscipit augue. Nam finibus gravida lorem eu viverra. Praesent rhoncus imperdiet ultricies. Nullam <br/> pretium cursus augue auctor vulputate. Quisque a convallis diam commodo eget diam id, eleifend dictum libero. Etiam varius, nisi vel dignissim sodales, enim dui posuere <br/> mauris, in aliquet lorem eros eget neque.</p>
+                  </div>
+
+                  <div className="tab-pane fade" id="pills-video" role="tabpanel" aria-labelledby="pills-video-tab">
+                    <h3>Video</h3>
+                    <div class="embed-responsive embed-responsive-16by9">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/5P31UruORjg" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                    </div>
+                  </div>
+
+                  <div className="tab-pane fade" id="pills-size" role="tabpanel" aria-labelledby="pills-size-tab">
+                    <h3>Size</h3>
+                      <p className="properties">Sed ut mi mollis, consequat nulla lacinia, hendrerit turpis. Nulla sapien magna, interdum quis pretium nec, pharetra at felis. Curabitur dictum sapien est, eget ultricies turpis <br/> porta vel. Nam suscipit nec lacus sed imperdiet. Vestibulum a purus risus. Nulla et dictum augue, nec efficitur mi. Nam sit amet pretium elit. Aliquam congue, ligula id <br/> vehicula vestibulum, orci ex vulputate lacus, ac malesuada elit dolor eget ex. Sed quis aliquet risus, ut cursus lectus. In eget lorem tellus.</p>
+                      <p className="properties">Quisque eleifend varius nisi nec sagittis. Nulla ullamcorper imperdiet justo, ut venenatis purus lobortis ut. Nunc sagittis urna et hendrerit sodales. Nunc molestie risus nec <br/> fringilla lacinia. Nulla facilisi. Etiam neque velit, tristique eget sollicitudin eget, placerat at metus. Proin dictum lobortis velit, id suscipit orci faucibus ut. Aliquam erat volut- <br/> pat. Vivamus feugiat justo in diam placerat, id dignissim elit auctor. Vestibulum scelerisque sem et lobortis ultricies. Morbi suscipit nulla urna. Suspendisse potenti. Nullam <br/> varius quam sed nisl dignissim, vel faucibus ipsum blandit. Vivamus at suscipit augue. Nam finibus gravida lorem eu viverra. Praesent rhoncus imperdiet ultricies. Nullam <br/> pretium cursus augue auctor vulputate. Quisque a convallis diam commodo eget diam id, eleifend dictum libero. Etiam varius, nisi vel dignissim sodales, enim dui posuere <br/> mauris, in aliquet lorem eros eget neque.</p>
+                  </div>
+
+                  <div className="tab-pane fade" id="pills-delivery" role="tabpanel" aria-labelledby="pills-delivery-tab">
+                    <h3>Delivery</h3>
+                      <p className="properties">Sed ut mi mollis, consequat nulla lacinia, hendrerit turpis. Nulla sapien magna, interdum quis pretium nec, pharetra at felis. Curabitur dictum sapien est, eget ultricies turpis <br/> porta vel. Nam suscipit nec lacus sed imperdiet. Vestibulum a purus risus. Nulla et dictum augue, nec efficitur mi. Nam sit amet pretium elit. Aliquam congue, ligula id <br/> vehicula vestibulum, orci ex vulputate lacus, ac malesuada elit dolor eget ex. Sed quis aliquet risus, ut cursus lectus. In eget lorem tellus.</p>
+                      <p className="properties">Quisque eleifend varius nisi nec sagittis. Nulla ullamcorper imperdiet justo, ut venenatis purus lobortis ut. Nunc sagittis urna et hendrerit sodales. Nunc molestie risus nec <br/> fringilla lacinia. Nulla facilisi. Etiam neque velit, tristique eget sollicitudin eget, placerat at metus. Proin dictum lobortis velit, id suscipit orci faucibus ut. Aliquam erat volut- <br/> pat. Vivamus feugiat justo in diam placerat, id dignissim elit auctor. Vestibulum scelerisque sem et lobortis ultricies. Morbi suscipit nulla urna. Suspendisse potenti. Nullam <br/> varius quam sed nisl dignissim, vel faucibus ipsum blandit. Vivamus at suscipit augue. Nam finibus gravida lorem eu viverra. Praesent rhoncus imperdiet ultricies. Nullam <br/> pretium cursus augue auctor vulputate. Quisque a convallis diam commodo eget diam id, eleifend dictum libero. Etiam varius, nisi vel dignissim sodales, enim dui posuere <br/> mauris, in aliquet lorem eros eget neque.</p>
+                  </div>
+
+                  <div className="tab-pane fade" id="pills-reviews" role="tabpanel" aria-labelledby="pills-reviews-tab">
+                    <h3>Reviews</h3>
+                      <p className="properties">Sed ut mi mollis, consequat nulla lacinia, hendrerit turpis. Nulla sapien magna, interdum quis pretium nec, pharetra at felis. Curabitur dictum sapien est, eget ultricies turpis <br/> porta vel. Nam suscipit nec lacus sed imperdiet. Vestibulum a purus risus. Nulla et dictum augue, nec efficitur mi. Nam sit amet pretium elit. Aliquam congue, ligula id <br/> vehicula vestibulum, orci ex vulputate lacus, ac malesuada elit dolor eget ex. Sed quis aliquet risus, ut cursus lectus. In eget lorem tellus.</p>
+                      <p className="properties">Quisque eleifend varius nisi nec sagittis. Nulla ullamcorper imperdiet justo, ut venenatis purus lobortis ut. Nunc sagittis urna et hendrerit sodales. Nunc molestie risus nec <br/> fringilla lacinia. Nulla facilisi. Etiam neque velit, tristique eget sollicitudin eget, placerat at metus. Proin dictum lobortis velit, id suscipit orci faucibus ut. Aliquam erat volut- <br/> pat. Vivamus feugiat justo in diam placerat, id dignissim elit auctor. Vestibulum scelerisque sem et lobortis ultricies. Morbi suscipit nulla urna. Suspendisse potenti. Nullam <br/> varius quam sed nisl dignissim, vel faucibus ipsum blandit. Vivamus at suscipit augue. Nam finibus gravida lorem eu viverra. Praesent rhoncus imperdiet ultricies. Nullam <br/> pretium cursus augue auctor vulputate. Quisque a convallis diam commodo eget diam id, eleifend dictum libero. Etiam varius, nisi vel dignissim sodales, enim dui posuere <br/> mauris, in aliquet lorem eros eget neque.</p>
+                  </div>
+                </div>
+              </div>
 
             </div>
           </div>
